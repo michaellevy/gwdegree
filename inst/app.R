4dbc988161da7548df09f083b2dce6cec42bb695
Michael Levy <malevy@ucdavis.edu>
2016-07-02 00:42:34 -0700

    removed all interpretation text

--- inst/app.R
+++ inst/app.R
@@ -9,21 +9,14 @@ cols = structure(rev(RColorBrewer::brewer.pal(length(gwdDecayOptions) + 2, 'BuPu
 changeStatTab =
   tabItem(tabName = "stat",
           h2("GW Degree Statistic Behavior"),
-          HTML("Plot the change to the GWD statistic for adding an edge to a node of given degree,
+          HTML("Plot the change to the GWD statistic for adding a half-edge to a node of given degree,
             at various decay-parameter (&theta;<sub>S</sub>) values.<br><br>
-            Three take-home points:"),
-          tags$ol(
-            tags$li(HTML("The statistic increases with any edge, and it always increases more for edge added to a
-                        lower-degree node than a higher-degree node. The implication is that for positive values of the corresponding
-                        parameter (&theta;<sub>GWD</sub>), the likelihood of an edge on a lower-degree node is greater than an edge on a high-degree node.
-                        This is discussed in more detail below the charts.")),
-            tags$li(HTML("&theta;<sub>S</sub> should never be less than zero.")),
-            tags$li(HTML("GWD always weights changes to lower-degree nodes more than changes to higher-degree nodes.
-                        The closer &theta;<sub>S</sub> is to zero, the more dramatic the difference. Thus, if you want
-                        &theta;<sub>GWD</sub> to capture a popularity effect that operates <em>among popular actors</em>, choose a large
-                        value for &theta;<sub>S</sub>. The particular value will depend on the size and density of the network -- the
-                        \"Parameter and Degree Distribution\" tab is meant to facilitate this choice."))
-          ),
+               The GWD statistic is given by:"),
+               withMathJax("$$GWD(\\mathbf{y}, \\theta_s) = e^{\\theta_s} \\sum_{k=1}^{n-1}[1-(1-e^{-\\theta_s})^{k}]D_{k}(\\mathbf{y})$$"),
+               withMathJax("Where \\(\\mathbf{y}\\) is the network of \\(n\\) nodes, \\(D_{k}\\) of which have degree-\\(k\\), and \\(\\theta_s\\)
+                      is a decay parameter that controls the severity of geometric weighting.
+                      Noting that adding a half-edge to a node of degree-k, increments \\(D_{k+1}\\) and decrements \\(D_{k}\\), the change statistic is:"),
+               withMathJax("$$\\delta GWD = (1 - e^{-\\theta_s})^k$$"),
           tags$br(), tags$br(),
 
           fluidRow(
@@ -41,7 +34,6 @@ changeStatTab =
                                    min = 0, max = 100,
                                    value = c(0, 20))
                    )
-
             ),
 
             column(width = 7,
@@ -50,34 +42,7 @@ changeStatTab =
                        plotOutput("statPlot")
                    )
             )
-          ),
-          tags$br(), tags$br(),
-          h3("Analytical Solution"),
-          "The GWD statistic is given by:",
-          withMathJax("$$GWD(\\mathbf{y}, \\theta_s) = e^{\\theta_s} \\sum_{k=1}^{n-1}[1-(1-e^{-\\theta_s})^{k}]D_{k}(\\mathbf{y})$$"),
-          withMathJax("Where \\(\\mathbf{y}\\) is the network of \\(n\\) nodes, \\(D_{k}\\) of which have degree-\\(k\\), and \\(\\theta_s\\)
-                    is a decay parameter that controls the severity of geometric weighting.
-                    The change statistic for adding a half-edge to a node of degree-k, then, is:"),
-          withMathJax("$$\\delta GWD = (1 - e^{-\\theta_s})^k$$"),
-          HTML("From the change statistic, we see why &theta;<sub>S</sub> < 0 makes interpretation of &theta;<sub>GWD</sub>
-                 difficult: the change statistic is positive or negative depending on whether the degree of the node
-                 receiving an edge has even- or odd-degree. Furthermore, we see that &theta;<sub>S</sub> < -ln(2) is deeply problematic:
-                 the exponentiated term is less than -1, so as k increases, the statistic explodes."),
-
-          h3("Implication"),
-          HTML("In an ERGM, the change in the log-odds of an edge is given by the
-            product of the change statistic and the parameter value (akin to a logistic regression).
-            So, for positive GWD parameters,
-            edges that increase the change statistic more are more likely; conversely,
-            for negative GWD parameter values, edges that
-            increase the change statistic less are more likely. Note in the plot above that edges to nodes of low degree increase the statistic more.
-            This implies that positive GWD parameters reward edges to low-degree nodes, and negative GWD parameters reward edges to higher-degree nodes.
-            Therefore, <b>negative GWD parameter estimates are consistent with networks more centralized (i.e. with higher-variance degree distributions)
-            than expected by chance, <i>ceteris paribus.</i></b> <br><br>
-            You may notice that for &theta;<sub>GWD</sub> < 0, all edges are
-            penalized and for &theta;<sub>GWD</sub> > 0, all edges are rewarded. This fact is offset by the inclusion of a
-            density or edges term in the model which establishes the baseline likelihood of any edge, or fixing the number
-            of edges in the network: It is the <i>relative</i> likelihood of edges on various nodes that is of interest.")
+          )
   )
 
 
@@ -88,26 +53,14 @@ degDistTab =
     h2("Parameter & Degree Distribution"),
     HTML("The two networks plotted below have the same number of nodes and edges,
              but the purple one is a random network (all edges equiprobable),
-            while the blue one is simulated with the
+            and the blue one is simulated with the
              selected GWD parameter and decay values. Adjust the sliders and click
             \"Simulate!\" to see how the blue
              network changes relative to the baseline of the purple one.<br><br>
              The histograms at right are average degree distributions from multiple
              networks simulated the same way as the plotted networks. You can examine
             degree centralization directly as a response to parameter values in the
-            \"Centralization, Clustering, & GWESP\" tab at left.<br><br>
-             Note that when &theta;<sub>GWD</sub> < 0, there are more high- and
-             low-degree nodes, and when &theta;<sub>GWD</sub> > 0, there are more
-             middle-degree nodes. That is, negative values of &theta;<sub>GWD</sub>
-             are consistent with a network more centralized than expected by chance.
-             The particular shape of the degree distribution
-             is a function of &theta;<sub>S</sub> (aka \"decay\"), as well as network
-             size and density. As &theta;<sub>S</sub> approaches zero, changes
-             to low-degree nodes have more of an effect on the statistic than changes
-             to high-degree nodes. See the \"Statistic Behavior\" tab for the intuition on this.
-             Increasing the number of simulated networks reduces
-             noise from stochastic simulation, but simulations can be computationally
-             expensive, especially for large and dense networks."),
+            \"Centralization, Clustering, & GWESP\" tab at left."),
     tags$br(), tags$br(),
 
     # Inputs
@@ -140,7 +93,7 @@ degDistTab =
           sliderInput("reps",
                       label = "Number of Simulated Networks", ticks = FALSE,
                       min = 3, max = 50,
-                      value = 10, step = 1),
+                      value = 20, step = 1),
           actionButton("goDD", label = strong("Simulate!"))
       )
     ),
@@ -165,31 +118,10 @@ gwespTab =
     h2("Confoundedness of Centralization & Clustering"),
     HTML("Here you can examine how network centralization (the tendency for edges to
         be concentrated on popular actors) and clustering (the tendency to form complete triangles)
-        respond across a range of not only GWD-, but also GWESP-parameter values, and
-        their respective decay parameters, as well as network size and density. To examine
-        just &theta;<sub>GWD</sub>'s effects, look across the horizontal
-        &theta;<sub>GWESP</sub> = 0 line. Note, as in the other tabs, that greater
-        values of &theta;<sub>GWD</sub> correspond to <i>less</i> centralized networks.<br><br>
-        Note also the confoundedness of clustering and centralization: Negative values
-        of &theta;<sub>GWD</sub> create centralized networks, where there are many
-        edges in close proximity, which leads to
-        complete triangles by chance. Likewise, positive &theta;<sub>GWESP</sub> values
-        create complete triangles, which puts edges near each other, incidentally causing
-        centralized networks. Thankfully, ERGMs that include
-        both GWD and GWESP parameters can distinguish the two effects to the extent
-        that they leave a distinctive structural signature in the network.
-        For this reason, modeling
-        efforts that include one GW-term should at least begin with both terms (and
-        probably GWDSP as well to distinguish tendency for two-paths from the tendency
-        for two-paths that become closed triangles.)
+        respond across a range of both GWD- and GWESP-parameter values, their respective decay parameters, and network size and density.
         <br><br>
-        Note that these simulations are computationally expensive. Especially for larger
-             and denser networks, the calculations may take minutes. Start slowly and work your way up. If you would
-             like to run these simulations for larger networks, consider cloning the app
-             and deploying it locally. Instructions and code are "),
-    tags$a(href="https://github.com/michaellevy/GWDegree-Shiny", "on GitHub"), ".",
-
-
+        Note that these simulations are computationally expensive. Larger
+             and denser networks will take minutes for each replicate network."),
     tags$br(), tags$br(),
 
     # Inputs
@@ -232,7 +164,7 @@ gwespTab =
       box(width = 3, title = "Simulation Parameters",
           status = "primary", solidHeader = TRUE,
           selectInput("reps3",
-                      label = "Number of Simulated Networks",
+                      label = "Number of Replicate Networks",
                       choices = as.list(1:3), selected = 3),
           selectInput("gridSize",
                       label = "Grid size",
@@ -268,7 +200,7 @@ header = dashboardHeader(title = "GW Degree")
 sidebar =
   dashboardSidebar(sidebarMenu(
     menuItem("Statistic Behavior", tabName = "stat"),
-    menuItem("Parameter & Degree Distribution", tabName = "degdist", selected = TRUE),
+    menuItem("Parameter & Degree Distribution", tabName = "degdist"),
     menuItem("Centralization, Clustering, & GWESP", tabName = "gwesp")
   ))
 
@@ -297,8 +229,8 @@ server =
     ### Change-statistic plot  ####
     deltaGWDdf = reactive({
       gwdegree:::makeDeltaGWDdF(kmin = input$degreeRange[1],
-                     kmax = input$degreeRange[2],
-                     decay = as.numeric(input$theta_s))
+                                kmax = input$degreeRange[2],
+                                decay = as.numeric(input$theta_s))
     })
     output$statPlot = renderPlot({
       gwdegree:::plotDeltaGWD(d = deltaGWDdf(), cols = cols)
@@ -431,13 +363,13 @@ server =
       input$goGWESP
       isolate({
         gwdegree:::simCCCent(gwdRange = input$gwd3,
-                  gwespRange = input$gwesp3,
-                  theta_s = input$theta_s3,
-                  theta_t = input$theta_t3,
-                  gridSize = as.integer(input$gridSize),
-                  nsim = as.integer(input$reps3),
-                  netSize = input$netSize3,
-                  density = input$density3)
+                             gwespRange = input$gwesp3,
+                             theta_s = input$theta_s3,
+                             theta_t = input$theta_t3,
+                             gridSize = as.integer(input$gridSize),
+                             nsim = as.integer(input$reps3),
+                             netSize = input$netSize3,
+                             density = input$density3)
       })
     })
     output$centHeatmap = renderPlot(heatmaps()$cent)
