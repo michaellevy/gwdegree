4ccd4a6140e4eb28e01b65e21192e2d0c4db9c6d
Michael Levy <malevy@ucdavis.edu>
2016-07-01 22:40:50 -0700

    distribution test

--- R/functions.R
+++ R/functions.R
@@ -1,7 +1,23 @@
-theme_set(theme_bw(base_size = 20))
+#' Run GW-Degree Shiny Application
+#'
+#' @return NULL Called for side-effect of launching shiny application
+#' @export
+#'
+#' @import ergm sna ggplot2 scales dplyr shiny shinydashboard
+#' @importFrom grDevices topo.colors
+#' @importFrom graphics plot
+#'
+#' @examples
+#' \dontrun{
+#' gwdegree()
+#' }
+gwdegree <- function() {
+  shiny::runApp(system.file('app.R', package='gwdegree'))
+  return(NULL)
+}
 
 plotDeltaGWD = function(d, cols) {
-    ggplot(d, aes(x = degree, y = delta_GWD, color = as.factor(theta_s))) + 
+    ggplot(d, aes(x = degree, y = delta_GWD, color = as.factor(theta_s))) +
         geom_line(size = 1) +
         ylab(expression(paste(delta, ' GWD'))) +
         xlab('Node degree') +
@@ -20,52 +36,31 @@ makeDeltaGWDdF = function(kmin = 0, kmax = 20, decay = 1) {
 
 deltaGWD = function(k, theta_s)  (1 - exp(-theta_s))^k
 
-makeRandomGraph = function(nodes, density, directed = FALSE)
-{
-    require(statnet)
-    
-    edges = density * nodes * (nodes - 1)
-    if(!directed) 
-        edges = edges / 2
-    
-    maxEdges = nodes * (nodes - 1) / abs(directed - 2)
-    if(edges >= maxEdges)
-        stop("Too many edges. I won't make you a complete graph!")
-    n = network.initialize(nodes, directed = directed)
-    ed = as.data.frame(t(replicate(edges, sort(sample(network.size(n), 2)))))
-    while(any(duplicated(ed))) {
-        for(i in which(duplicated(ed)))
-            ed[i, ] = sort(sample(network.size(n), 2))
-    }
-    add.edges(n, ed$V1, ed$V2)  
-    n
-}
-
 plotNet = function(net, vCol, mtext) {
-    plot(net, 
-         vertex.col = vCol, 
+    plot(net,
+         vertex.col = vCol,
          pad = 0,
-         edge.col = 'gray', 
+         edge.col = 'gray',
          vertex.border = 'white',
          vertex.lwd = .35,
          vertex.cex = 4 - log10(network.size(net))
-         ) 
+         )
     mtext(mtext, cex = 1.5)
 }
 
 simCCCent = function(gwdRange = c(-2, 2), gwespRange = c(-.5, .5),
                      theta_s = 1.5, theta_t = .5, gridSize = 3, nsim = 1,
                      netSize = 100, meanDegree = 3) {
-    
+
     dfForSim = expand.grid(
             gwd = seq(gwdRange[1], gwdRange[2], len = gridSize),
             gwesp = seq(gwespRange[1], gwespRange[2], len = gridSize))
-    
+
     N = intergraph::asNetwork(igraph::erdos.renyi.game(netSize, netSize * meanDegree, "gnm"))
-    
-    dfForSim = 
+
+    dfForSim =
         lapply(1:nrow(dfForSim), function(i) {
-            n = simulate.formula(N ~ gwdegree(theta_s, TRUE) + gwesp(theta_t, TRUE), 
+            n = simulate.formula(N ~ gwdegree(theta_s, TRUE) + gwesp(theta_t, TRUE),
                                  coef = unlist(dfForSim[i, ]),
                                  constraints = ~ edges,
                                  nsim = nsim)
@@ -76,9 +71,9 @@ simCCCent = function(gwdRange = c(-2, 2), gwespRange = c(-.5, .5),
         }) %>%
         do.call(rbind, .) %>%
         cbind(dfForSim, .)
-    
+
     plotHeatmaps(dfForSim)
-    
+
 }
 
 clusteringCoef = function(net)
@@ -86,25 +81,20 @@ clusteringCoef = function(net)
 
 plotHeatmaps = function(df) {
     list(
-        cent = 
+        cent =
             ggplot(df, aes(x = gwd, y = gwesp, fill = Centralization)) +
             geom_raster(alpha = .7) +
             scale_fill_gradientn(colours = topo.colors(1e3), name = "") +
-            # scale_fill_gradient2(name = 'Degree\ncentralization', mid = 'lightgray', 
-            #                      midpoint = strSims$Centralization[strSims$GWD == 0 & strSims$GWESP == 0]) +
             xlab(expression(theta[GWD])) +
             ylab(expression(theta[GWESP])) +
             theme(aspect.ratio = 1),
-        
-        cc = 
+
+        cc =
             ggplot(df, aes(x = gwd, y = gwesp, fill = ClusteringCoef)) +
             geom_raster(alpha = .7) +
             scale_fill_gradientn(colours = topo.colors(1e3), name = "") +
-            # scale_fill_gradient2(name = 'Clustering\nCoefficient', mid = 'lightgray', 
-            #                      midpoint = strSims$ClusteringCoef[strSims$GWD == 0 & strSims$GWESP == 0]) +
             xlab(expression(theta[GWD])) +
             ylab(expression(theta[GWESP])) +
             theme(aspect.ratio = 1)
     )
-# cowplot::plot_grid(cent, cc, labels = letters[1:2], align = 'vh')
 }
